f1c39a26476110462b2589c17e0d82111faf96ff
Leaf-Oct <leafoct@gmail.com>
2024-10-13 17:09:01 +0800

    first commit

---
+++ src/main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math/rand"
+	"os"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/getlantern/systray"
+	"golang.design/x/clipboard"
+)
+
+const (
+	MAX_PATH = 260
+	WINDOWS  = "windows"
+	NOGUI    = "nogui"
+	LINUX    = "linux"
+)
+
+//go:embed icon.ico
+var icon []byte
+var local_os string
+
+func detectOS() string {
+	local_os = runtime.GOOS
+	switch local_os {
+	case WINDOWS:
+		break
+	case LINUX:
+		//获取环境变量DISPLAY
+		display := os.Getenv("DISPLAY")
+		if display == "" {
+			local_os = NOGUI
+		}
+	default:
+		fmt.Println("没做适配，自求多福吧")
+	}
+	return local_os
+}
+
+func noSingleProcess() bool {
+	switch local_os {
+	case WINDOWS:
+		_, err := checkSingletonWindows()
+		return err == syscall.ERROR_ALREADY_EXISTS
+	case LINUX:
+		//TODO
+	}
+	return false
+}
+
+func messageBox(title string, content string) {
+	switch local_os {
+	case WINDOWS:
+		messageBoxWindows(title, content)
+	case LINUX:
+		//TODO
+	}
+
+}
+
+func main() {
+	local_os := detectOS()
+	if local_os == NOGUI {
+		fmt.Println("当前环境无GUI，无法弹出消息框，请自行查看错误信息")
+		return
+	}
+	if noSingleProcess() {
+		messageBox("错误", "程序已运行。看右下角系统托盘(Windows)")
+		return
+	}
+	// 设置systray
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+
+	systray.SetIcon(icon)
+	systray.SetTitle("Mix")
+	systray.SetTooltip("图片像素混淆器")
+
+	mixItem := systray.AddMenuItem("混淆", "从剪切板获取图像，修改像素，写回剪切板")
+	saveItem := systray.AddMenuItem("保存", "保存剪切板图像到本地")
+	aboutItem := systray.AddMenuItem("关于", "关于软件")
+	exitItem := systray.AddMenuItem("退出", "退出程序")
+
+	go func() {
+		for {
+			select {
+			case <-mixItem.ClickedCh:
+				mix()
+			case <-aboutItem.ClickedCh:
+				messageBox("关于", "本程序功能为，从剪切板读取图像(jpg, png, bmp, webp)，随机修改若干像素，写回剪切板\n十月叶~Leaf Oct 开发")
+			case <-exitItem.ClickedCh:
+				systray.Quit()
+			case <-saveItem.ClickedCh:
+				save()
+			}
+		}
+	}()
+}
+
+func onExit() {
+	// 清理工作可以在这里进行
+}
+
+func save() {
+	img_bytes := clipboard.Read(clipboard.FmtImage)
+	if len(img_bytes) == 0 {
+		messageBox("错误", "复制的不是图像")
+		return
+	}
+	filename := fmt.Sprintf("%d", time.Now().UnixMilli()) + ".png"
+	filter := "Image Files (*.png)\\0*.png\\0"
+	SaveBytesWindows(filename, filter, img_bytes)
+	img_bytes = nil
+	runtime.GC()
+}
+
+func mix() {
+	img_bytes := clipboard.Read(clipboard.FmtImage)
+	if len(img_bytes) == 0 {
+		messageBox("错误", "复制的不是图像")
+		return
+	}
+	img, err := png.Decode(bytes.NewReader(img_bytes))
+	if err != nil {
+		messageBox("解析图像错误", "读取剪切板图像后转化的格式不是png。问题可能出在clipboard库")
+		return
+	}
+	bound := img.Bounds()
+	width := bound.Max.X
+	height := bound.Max.Y
+	new_image := image.NewRGBA64(bound)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			new_image.SetRGBA64(x, y, color.RGBA64{R: uint16(r), G: uint16(g), B: uint16(b), A: uint16(a)})
+			fmt.Println(x, y, " : ", r, g, b)
+		}
+	}
+	// 随机更改五个像素的值
+	for i := 0; i < 5; i += 1 {
+		random_x := rand.Intn(width)
+		random_y := rand.Intn(height)
+		pixel := new_image.At(random_x, random_y)
+		r, g, b, a := pixel.RGBA()
+		r = rand.Uint32()
+		g = rand.Uint32()
+		b = rand.Uint32()
+		changed_pixel := color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+		new_image.Set(random_x, random_y, changed_pixel)
+	}
+	var modified_png bytes.Buffer
+	err = png.Encode(&modified_png, new_image)
+	if err != nil {
+		messageBox("失败", "混淆后的图片无法编码")
+		return
+	}
+	clipboard.Write(clipboard.FmtImage, modified_png.Bytes())
+
+	// 不放心，手动释放下内存
+	img_bytes = nil
+	img = nil
+	new_image = nil
+	runtime.GC()
+}
+
+// debug使用，在正式发布版本中不会用到
+func writeOut(bytes []byte) {
+	file, _ := os.Create("output.png")
+	file.Write(bytes)
+	file.Close()
+}
---
+++ src/windows.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+func messageBoxWindows(title, text string) {
+	user32 := windows.NewLazySystemDLL("user32")
+	MessageBox := user32.NewProc("MessageBoxW")
+
+	var title16, text16 *uint16
+	if title != "" {
+		title16, _ = syscall.UTF16PtrFromString(title)
+	}
+	text16, _ = syscall.UTF16PtrFromString(text)
+
+	MessageBox.Call(uintptr(0), uintptr(unsafe.Pointer(text16)), uintptr(unsafe.Pointer(title16)), uintptr(windows.MB_OK))
+}
+
+func checkSingletonWindows() (windows.Handle, error) {
+	path, err := os.Executable()
+	if err != nil {
+		return 0, err
+	}
+	hashName := md5.Sum([]byte(path))
+	name, err := syscall.UTF16PtrFromString("Local\\" + hex.EncodeToString(hashName[:]))
+	if err != nil {
+		return 0, err
+	}
+	return windows.CreateMutex(nil, false, name)
+}
+
+// 这些是Windows API相关的常量
+const (
+	OFN_EXPLORER        = 0x00080000
+	OFN_FILEMUSTEXIST   = 0x00001000
+	OFN_HIDEREADONLY    = 0x00000004
+	OFN_OVERWRITEPROMPT = 0x00000002
+)
+
+// OPENFILENAME结构体
+type OPENFILENAME struct {
+	lStructSize       uint32
+	hwndOwner         uintptr
+	hInstance         uintptr
+	lpstrFilter       *uint16
+	lpstrCustomFilter *uint16
+	nMaxCustFilter    uint32
+	nFilterIndex      uint32
+	lpstrFile         *uint16
+	nMaxFile          uint32
+	lpstrFileTitle    *uint16
+	nMaxFileTitle     uint32
+	lpstrInitialDir   *uint16
+	lpstrTitle        *uint16
+	flags             uint32
+	nFileOffset       uint16
+	nFileExtension    uint16
+	lpstrDefExt       *uint16
+	lCustData         uintptr
+	lpfnHook          uintptr
+	lpTemplateName    *uint16
+}
+
+var (
+	modcomdlg32         = syscall.NewLazyDLL("Comdlg32.dll")
+	procGetSaveFileName = modcomdlg32.NewProc("GetSaveFileNameW")
+)
+
+func utf16Ptr(s string) *uint16 {
+	return (*uint16)(unsafe.Pointer(syscall.StringToUTF16Ptr(s)))
+}
+
+func saveFileDialog(defaultFileName, filter string) (string, error) {
+	ofn := OPENFILENAME{
+		lStructSize:  uint32(unsafe.Sizeof(OPENFILENAME{})),
+		hwndOwner:    0,
+		hInstance:    0,
+		lpstrFilter:  utf16Ptr(filter + "\\0\\0"),
+		nFilterIndex: 1,
+		lpstrFile:    utf16Ptr(defaultFileName),
+		nMaxFile:     256,
+		flags:        OFN_EXPLORER | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT,
+	}
+	r, _, _ := procGetSaveFileName.Call(uintptr(unsafe.Pointer(&ofn)))
+	if r == 0 {
+		return "", fmt.Errorf("User canceled the operation or an error occurred.")
+	}
+	return syscall.UTF16ToString((*[256]uint16)(unsafe.Pointer(ofn.lpstrFile))[:]), nil
+}
+
+// SaveBytesToFile 保存字节数组到用户选择的文件
+func SaveBytesWindows(defaultFileName, filter string, data []byte) error {
+	filePath, err := saveFileDialog(defaultFileName, filter)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return fmt.Errorf("Failed to write to file: %v", err)
+	}
+
+	fmt.Printf("The content was written to %s\n", filePath)
+	return nil
+}
